eb3853dcfd6cbe68e659df79dc604741a7b5d025
EnlightGuru <ritesh.saw@outlook.com>
2020-12-17 15:59:59 +0530

    Add project files.

---
+++ Hooks/Hooks.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SpecflowNUnitTestProject.Hooks
+{
+    [Binding]
+    public sealed class Hooks
+    {
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+
+        public IWebDriver Driver;
+
+        public Hooks(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            Driver.Quit();
+        }
+    }
+}
